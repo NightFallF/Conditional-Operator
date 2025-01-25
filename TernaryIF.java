b83f499d942bf0957591bd6f875ef41f201bb293
NightFallF <nightfallzadh@gmail.com>
2025-01-25 13:27:01 +0530

    Add files via upload

---
+++ TernaryIF.java
@@ -0,0 +1,7 @@
+public class TernaryIF {
+    public static void main(String[] args) {
+        int n = 12;
+        String result = (n % 2 == 0) ? "Even!" : "Odd!";
+        System.out.println(result);
+    }
+}
